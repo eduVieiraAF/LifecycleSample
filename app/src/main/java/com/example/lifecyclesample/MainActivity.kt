0176bc4c92aff51d7cb68a95b5a3a6cd9e14f15c
eduVieiraAF <edu_vieira@hotmail.com>
2022-06-25 16:08:14 -0300

    Extra info
    Ways to add or pass extra info

--- app/src/main/java/com/example/lifecyclesample/MainActivity.kt
+++ app/src/main/java/com/example/lifecyclesample/MainActivity.kt
@@ -17,6 +17,8 @@ class MainActivity : AppCompatActivity() {
 
         btnSubmit.setOnClickListener {
             val intent = Intent(this, AnotherActivity::class.java)
+                .apply { putExtra("Key1", "Value1") } // adding and passing extra info
+            intent.putExtra("Key2", true) // another way to add pass extra info
             startActivity(intent)
         }
     }
